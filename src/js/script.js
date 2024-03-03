03347a17d3f39117dc80f4c09f2dec79f28bdf81
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-03-03 12:30:44 +0300

    finish project

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// код для слайдера в reviews
+
 const swiper = new Swiper('.mySwiper', {
     // Параметры по умолчанию
     slidesPerView: 3,
@@ -14,3 +16,23 @@ const swiper = new Swiper('.mySwiper', {
     },
 
 });
+// добавленния класса active
+document.querySelector('.burger').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('open');
+})
+
+// скроллинг
+
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.nav__link'); // сприсок всех nav nav__link
+
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const tardetID = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(tardetID);
+            targetSection.scrollIntoView({behavior: 'smooth'})
+        })
+    })
+})
